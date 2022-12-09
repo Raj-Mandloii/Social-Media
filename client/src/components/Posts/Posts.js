a5b27f200cf4a793d0112bf1d8885894a018a380
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-12-09 13:59:06 +0530

    app bar added

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Post } from './Post/Post.js'
+
+export const Posts = () => {
+    return (
+        <>
+            <div>Posts</div>
+            <Post />
+            <Post />
+        </>
+    )
+}
