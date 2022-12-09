f6533839c4eea1fe778092b3cf035e56d5cf3d14
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-12-09 13:24:03 +0530

    folder structure added and post api added

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,4 +1,28 @@
-export const getPosts = (req,res)=>{
+import PostMessage from "../models/postMessage.js"
 
-    res.send("THIS WORKS")
-} 
+
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await postMessage.find();
+       
+        res.status(200).json(postMessages)
+    } catch (e) {
+        res.status(400).json({message: e.message})
+    }
+
+}
+
+
+export const createPost = async(req, res) => {
+    
+    const body = req.body
+    const newPost = new PostMessage(post);
+    try{
+        await newPost.save()
+        res.status(201).json(newPost)
+    }
+    catch (e){
+        res.status(409).json({message: e.message})
+    }
+}
